38fafc7da121412a265246fb41b626c0492bafbc
KyleAktr <aktr.contact@gmail.com>
2023-10-17 18:29:28 +0200

    Implemented question display, single-answer selection, answer validation, and 'Next' button handling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./components/Home";
+import Questions from "./components/Questions";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <Questions />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Questions.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import quizQuestions from "../data/questions.json"
+
+const Questions = () => {
+
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+    const [selectedAnswer, setSelectedAnswer] = useState(null)
+    const [answerValid, setAnswerValid] = useState(false)
+    const currentQuestion = quizQuestions.quizQuestions[currentQuestionIndex]
+
+    const handleNextQuestion = () => {
+        if (currentQuestionIndex < quizQuestions.quizQuestions.length -1) {
+            setCurrentQuestionIndex(currentQuestionIndex + 1)
+            setSelectedAnswer(null)
+            setAnswerValid(false)
+        }
+    }
+
+    const handleAnswerChange = (e) => {
+        setSelectedAnswer(e.target.value)
+        setAnswerValid(true)
+    }
+
+    return (
+        <div>
+            <h2>Questions {currentQuestionIndex + 1} :</h2>
+        
+            <p>{currentQuestion.question}</p>
+
+            <form>
+                <label>
+                    <input 
+                    type="radio" 
+                    value={currentQuestion.options[0]} 
+                    checked={selectedAnswer === currentQuestion.options[0]}
+                    onChange={handleAnswerChange}
+                    />{currentQuestion.options[0]}
+                </label>
+                <label>
+                    <input 
+                    type="radio" 
+                    value={currentQuestion.options[1]} 
+                    checked={selectedAnswer === currentQuestion.options[1]}
+                    onChange={handleAnswerChange}
+                    />{currentQuestion.options[1]}
+                </label>
+                <label>
+                    <input 
+                    type="radio" 
+                    value={currentQuestion.options[2]} 
+                    checked={selectedAnswer === currentQuestion.options[2]}
+                    onChange={handleAnswerChange}
+                    />{currentQuestion.options[2]} 
+                </label>
+                <label>
+                    <input 
+                    type="radio" 
+                    value={currentQuestion.options[3]} 
+                    checked={selectedAnswer === currentQuestion.options[3]}
+                    onChange={handleAnswerChange}
+                    />{currentQuestion.options[3]}
+                </label>
+            </form>
+
+            <p>{currentQuestion.correctAnswer}</p>
+
+            <button onClick={handleNextQuestion} disabled={!answerValid}>Next</button>
+
+            
+
+        </div>
+    );
+};
+
+export default Questions;
