22a7c61951e3966497e0cc44c5f4eaccde3ec2e0
KyleAktr <aktr.contact@gmail.com>
2023-10-18 11:56:15 +0200

    Updated structure, resolved goodAnswer display issue, and added finish button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./components/Home";
 import Questions from "./components/Questions";
+import Results from "./components/Results";
 
 const App = () => {
+  const [goodAnswerCount, setGoodAnswerCount] = useState(0);
+
   return (
     <div>
       <Home />
-      <Questions />
+      <Questions
+        goodAnswerCount={goodAnswerCount}
+        setGoodAnswerCount={setGoodAnswerCount}
+      />
+      <Results goodAnswerCount={goodAnswerCount} />
     </div>
   );
 };
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,83 +1,90 @@
-import React, { useState } from 'react';
-import quizQuestions from "../data/questions.json"
+import React, { useState } from "react";
+import quizQuestions from "../data/questions.json";
 
-const Questions = () => {
+const Questions = ({ goodAnswerCount, setGoodAnswerCount}) => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [answerValid, setAnswerValid] = useState(false);
 
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-    const [selectedAnswer, setSelectedAnswer] = useState(null)
-    const [answerValid, setAnswerValid] = useState(false)
-    const [goodAnswer, setGoodAnswer] = useState(0)
-    const currentQuestion = quizQuestions.quizQuestions[currentQuestionIndex]
+  const currentQuestion = quizQuestions.quizQuestions[currentQuestionIndex];
 
-    const handleNextQuestion = () => {
-        if (currentQuestionIndex < quizQuestions.quizQuestions.length -1) {
-            setCurrentQuestionIndex(currentQuestionIndex + 1)
-            setSelectedAnswer(null)
-            setAnswerValid(false)
-        } if (selectedAnswer == currentQuestion.correctAnswer) {
-            // alert("Correct")
-                setGoodAnswer(goodAnswer + 1)
-        }
-
-        }
-    
-
-    const handleAnswerChange = (e) => {
-        setSelectedAnswer(e.target.value)
-        setAnswerValid(true)
+  const handleNextQuestion = () => {
+    if (currentQuestionIndex < quizQuestions.quizQuestions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+      setSelectedAnswer(null);
+      setAnswerValid(false);
     }
+    if (selectedAnswer == currentQuestion.correctAnswer) {
+      // alert("Correct")
+      setGoodAnswerCount(goodAnswerCount + 1);
+    }
+    if (currentQuestionIndex === quizQuestions.quizQuestions.length - 2) {
+      document.querySelector(".next-btn").innerHTML = "Terminer le quiz";
+    }
+  };
 
-    return (
-        <div>
-            <h2>Questions {currentQuestionIndex + 1} :</h2>
-        
-            <p>{currentQuestion.question}</p>
+  const handleAnswerChange = (e) => {
+    setSelectedAnswer(e.target.value);
+    setAnswerValid(true);
+  };
 
-            <form>
-                <label>
-                    <input 
-                    type="radio" 
-                    value={currentQuestion.options[0]} 
-                    checked={selectedAnswer === currentQuestion.options[0]}
-                    onChange={handleAnswerChange}
-                    />{currentQuestion.options[0]}
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    value={currentQuestion.options[1]} 
-                    checked={selectedAnswer === currentQuestion.options[1]}
-                    onChange={handleAnswerChange}
-                    />{currentQuestion.options[1]}
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    value={currentQuestion.options[2]} 
-                    checked={selectedAnswer === currentQuestion.options[2]}
-                    onChange={handleAnswerChange}
-                    />{currentQuestion.options[2]} 
-                </label>
-                <label>
-                    <input 
-                    type="radio" 
-                    value={currentQuestion.options[3]} 
-                    checked={selectedAnswer === currentQuestion.options[3]}
-                    onChange={handleAnswerChange}
-                    />{currentQuestion.options[3]}
-                </label>
-            </form>
+  return (
+    <div>
+      <h2>Questions {currentQuestionIndex + 1} :</h2>
 
-            {/* <p>{currentQuestion.correctAnswer}</p> */}
+      <p>{currentQuestion.question}</p>
 
-            <button onClick={handleNextQuestion} disabled={!answerValid}>Next</button>
+      <form>
+        <label>
+          <input
+            type="radio"
+            value={currentQuestion.options[0]}
+            checked={selectedAnswer === currentQuestion.options[0]}
+            onChange={handleAnswerChange}
+          />
+          {currentQuestion.options[0]}
+        </label>
+        <label>
+          <input
+            type="radio"
+            value={currentQuestion.options[1]}
+            checked={selectedAnswer === currentQuestion.options[1]}
+            onChange={handleAnswerChange}
+          />
+          {currentQuestion.options[1]}
+        </label>
+        <label>
+          <input
+            type="radio"
+            value={currentQuestion.options[2]}
+            checked={selectedAnswer === currentQuestion.options[2]}
+            onChange={handleAnswerChange}
+          />
+          {currentQuestion.options[2]}
+        </label>
+        <label>
+          <input
+            type="radio"
+            value={currentQuestion.options[3]}
+            checked={selectedAnswer === currentQuestion.options[3]}
+            onChange={handleAnswerChange}
+          />
+          {currentQuestion.options[3]}
+        </label>
+      </form>
 
-            <p>Nombre de bonne réponses : {goodAnswer}</p>
+      {/* <p>{currentQuestion.correctAnswer}</p> */}
 
-            
+      <button
+        className="next-btn"
+        onClick={handleNextQuestion}
+        disabled={!answerValid}
+      >
+        Next
+      </button>
 
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Questions;
