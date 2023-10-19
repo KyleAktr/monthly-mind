741e0edb633c1b72bb4de6a4e80db470b3656fc3
KyleAktr <aktr.contact@gmail.com>
2023-10-19 13:39:38 +0200

    results page style done

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React from "react";
+import Arrow from "../style/assets/img/arrow-down-solid.svg"
 
 const Results = ({ goodAnswerCount }) => {
+  const percentGoodAnswer = (goodAnswerCount / 8) * 100;
 
-    const percentGoodAnswer = (goodAnswerCount / 8)  * 100
-
-    return (
-        <div>
-            <h2>Results</h2>
-            <p>Nombre de bonnes réponses : {goodAnswerCount}</p>
-            <p>Pourcentage de bonne réponses : {percentGoodAnswer}</p>
+  return (
+    <div className="results-container">
+      <div className="results-card-container">
+        <h2>Résultats</h2>
+        <div className="results-card">
+          <div className="results">
+            <div className="results-stats">
+                <p>Nombre de bonnes réponses : {goodAnswerCount}/8</p>
+                <p>Pourcentage de bonne réponses : {percentGoodAnswer}%</p>
+                <p className="congrats">Bravo !!!</p>
+            </div>
+          </div>
         </div>
-    );
+        <div className="show-article">
+            <p>Retrouve toutes les réponses dans l’article ci dessous </p>
+            <img className="arrow" src={Arrow} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Results;
